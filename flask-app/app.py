84dec18b8875935a274205c59aee3519b3145981
DolevTB <dolevgeva@gmail.com>
2025-01-28 16:01:06 +0200

    Update app.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 def helloworld():
     return "Hello World!"
 if __name__ == "__main__":
-    app.run(debug=True, port=8080, host='170.0.0.1')
+    app.run(host='0.0.0.0', port=8080, debug=True)
